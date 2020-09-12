1a7fba983496ed5f84142e02c03553f5ec17f6a0
Tushar912 <tushar.912u@gmail.com>
2020-09-12 18:04:01 +0530

    lesson 5 done .

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.homepage),
-    path('about/',views.about)
+    path('about/',views.about),
     path('articles/',include('articles.urls'))
 ]
