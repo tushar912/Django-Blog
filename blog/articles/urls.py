db177d4e962de79afc283440fdfe2f5dffea7b02
Tushar912 <tushar.912u@gmail.com>
2020-09-12 22:56:16 +0530

    add

--- blog/articles/urls.py
+++ blog/articles/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from django.conf.urls import url
 from . import views
 
+app_name = 'articles'
 urlpatterns = [
-    path('', views.article_list,name='list'),
+   
+    url(r'^$', views.article_list, name="list"),
     url(r'^(?P<slug>[\w-]+)/$', views.article_detail,name='detail'),
 ]
--- blog/articles/views.py
+++ blog/articles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Article
 
 def article_list(request):
