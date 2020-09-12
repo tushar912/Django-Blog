6df32f51c0ffc01896a1fe9a3b24043a199c81af
Tushar912 <tushar.912u@gmail.com>
2020-09-12 21:45:39 +0530

    done 12

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -3,9 +3,13 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.homepage),
     path('about/',views.about),
     path('articles/',include('articles.urls'))
 ]
+
+urlpatterns += staticfiles_urlpatterns()
