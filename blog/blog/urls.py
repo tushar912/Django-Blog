5bb77ec758e13f57fc78c2b59b677b95ba0637b9
Tushar912 <tushar.912u@gmail.com>
2020-09-12 17:30:24 +0530

    changed url to path

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,10 +1,10 @@
 
-from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path
 from . import views
 
 urlpatterns = [
-     url(r'^admin/', admin.site.urls),
-    url(r'^about/$', views.about),
-    url(r'^$', views.homepage),
+    path('admin/', admin.site.urls),
+    path('',views.homepage),
+    path('about/',views.about)
 ]
