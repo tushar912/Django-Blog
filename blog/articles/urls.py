2eb98ca554b63e03a3b553991506a3f0a35e13d3
Tushar912 <tushar.912u@gmail.com>
2020-09-12 22:21:51 +0530

    lesson 15

--- blog/articles/urls.py
+++ blog/articles/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.article_list),
-    url(r'^(?P<slug>[\w-]+)/$', views.article_detail),
+    path('', views.article_list,name='list'),
+    url(r'^(?P<slug>[\w-]+)/$', views.article_detail,name='detail'),
 ]
