d3dc8f10781b1033fb14deb4873fdb796a138714
Tushar912 <tushar.912u@gmail.com>
2020-09-12 22:10:31 +0530

    lesson 14

--- blog/articles/urls.py
+++ blog/articles/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     path('', views.article_list),
+    url(r'^(?P<slug>[\w-]+)/$', views.article_detail),
 ]
--- blog/articles/views.py
+++ blog/articles/views.py
@@ -4,3 +4,7 @@ from .models import Article
 def article_list(request):
     articles= Article.objects.all().order_by('date') 
     return render(request, 'articles/article_list.html',{'articles':articles})
+
+def article_detail(request, slug):
+    return HttpResponse(slug)
+
