dc50797525644c09adc4c78d7df06e02154bd95f
Tushar912 <tushar.912u@gmail.com>
2020-09-13 22:49:06 +0530

    done 22

--- blog/accounts/views.py
+++ blog/accounts/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 def signup_view(request):
     if request.method == 'POST':
          form = UserCreationForm(request.POST)
          if form.is_valid():
              form.save()
+             user = form.get_user()
+             login(request, user)
              #  log the user in
              return redirect('articl:list')
     else:
