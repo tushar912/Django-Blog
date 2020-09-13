ceaa478ab1e78af402eaf18dd6adb5472b020780
Tushar912 <tushar.912u@gmail.com>
2020-09-13 22:47:35 +0530

    done 21

--- blog/accounts/views.py
+++ blog/accounts/views.py
@@ -11,3 +11,13 @@ def signup_view(request):
     else:
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', { 'form': form })
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # log the user in
+            return redirect('articles:list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', { 'form': form })
