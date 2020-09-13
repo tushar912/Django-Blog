5dc017190c5cb0293e45f76d84a6a8449951f22c
Tushar912 <tushar.912u@gmail.com>
2020-09-13 21:29:11 +0530

    done 17

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -4,6 +4,9 @@ from django.urls import path,include
 from . import views
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,3 +16,4 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
