93b33149cf0c27cdfd8cf8261cb5f1f0ed0c2476
Tushar912 <tushar.912u@gmail.com>
2020-09-14 16:30:33 +0530

    done 27

--- blog/articles/models.py
+++ blog/articles/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 
 class Article(models.Model):
@@ -7,6 +9,8 @@ class Article(models.Model):
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     thumb = models.ImageField(default='default.png', blank=True)
+    author = models.ForeignKey(User, default=None,blank=True ,on_delete=models.CASCADE,)
+
     # add in author later
 
     def __str__(self):
--- blog/articles/views.py
+++ blog/articles/views.py
@@ -17,7 +17,9 @@ def article_create(request):
     if request.method == 'POST':
         form = forms.CreateArticle(request.POST, request.FILES)
         if form.is_valid():
-            # save article to db
+            instance = form.save(commit=False)
+            instance.author = request.user
+            instance.save()
             return redirect('articles:list')
     else:
         form = forms.CreateArticle()
